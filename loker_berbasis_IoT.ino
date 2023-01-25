740dd8012bb1592416d7875aac043e74f4e8c545
Kevin Arnandes <83899571+Vinzzztty@users.noreply.github.com>
2023-01-25 09:38:14 +0700

    update code, dan mengganti sensor ultrasonik dengan limit switch

--- loker_berbasis_IoT.ino
+++ loker_berbasis_IoT.ino
@@ -1,73 +1,42 @@
-#define BLYNK_PRINT Serial
+#include <Servo.h>
 #include <ESP8266WiFi.h>
 #include <BlynkSimpleEsp8266.h>
-#include <Servo.h>
-#include <SPI.h>
 #include <ThingSpeak.h>
 #include <WiFiClient.h>
 
-char Id[]="Bylnk_ID";
-char Wifi[]="SSID";
-char Pass[]="PASS_SSID";
+#define BLYNK_PRINT Serial
+
+char Id[]="5Gj7Rt9VmZac10WsSD580ofVWEXjPast";
+char Wifi[]="Iki";
+char Pass[]="pecelenak";
 
 // Thingspeak
-char* apiTulis = "WRITE_API_THINGSPEAK";
-char* apiBaca = "READ_API_THINGSPEAK";
-const long channelID = 2005298;
+char* apiTulis = "PEG4MYXQ7ATMSXTR";
+char* apiBaca = "8LUV1XASOY1CG7JS";
+const long channelID = 2006798;
+
+int button1 = D2;
+int buttonOn = 0;
+int ledMerah = D5;
+int ledIjo = D7;
+int buzzer = D8;
+int virtualValue = 0;   //Digunakan untuk menyimpan nilai Virtual PIN pada Blynk
 
 Servo myservo;
 WiFiClient client;
 
-int led_kuning = D6;
-int led_hijau = D7;
-int led_merah = D5;
-int buzzer = D8;
-int sensor = 0;
-int virtualValue = 0;
-
-
-long bacaJarak(int trigPin, int echoPin) 
-{
-  pinMode(trigPin, OUTPUT); //Set trigger as outpu
-  digitalWrite(trigPin, LOW); //generate frekuense set pin LOW
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(100);
-  digitalWrite(trigPin, LOW);
-  pinMode(echoPin, INPUT);
-  return pulseIn(echoPin, HIGH); //reads the echo PIN
-}
-
-BLYNK_WRITE(V2) // virtual pin 2 on blynk app
-{
-    int pinValue = param.asInt();
-    virtualValue = pinValue;
-    if (pinValue == 1) {//if button on 
-      myservo.write(0);// move servo to 58•
-      digitalWrite(led_merah, LOW);
-      digitalWrite(led_hijau, HIGH);
-      tone(buzzer, 13500);
-      delay(500);
-      tone(buzzer, 0);
-    } else {
-        myservo.write(180); // move servo to 0•
-        digitalWrite(led_merah, HIGH);
-        digitalWrite(led_hijau, LOW);
-        tone(buzzer, 13500);
-        delay(500);
-        tone(buzzer, 0);
-        delay(1000);
-    }
-}
-
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
-  pinMode(led_hijau, OUTPUT);
-  pinMode(led_merah, OUTPUT);
-  pinMode(led_kuning, OUTPUT);
+
+  pinMode(button1, INPUT);
+  pinMode (ledMerah, OUTPUT);
+  pinMode (ledIjo, OUTPUT);
   pinMode(buzzer, OUTPUT);
+
   myservo.attach(D9);
   myservo.write(180);
+
+  Blynk.begin(Id,Wifi,Pass,"iot.amikom.ac.id",8080);
+  Serial.begin(9600);
   WiFi.begin(Wifi, Pass);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
@@ -77,43 +46,66 @@ void setup() {
   Serial.print("Connected, IP Address: ");
   Serial.println(WiFi.localIP());
   Serial.println("OK MULAI");
-  delay(1000);
-  Serial.println("LABEL,Data");
-  Blynk.begin(Id,Wifi,Pass,"iot.amikom.ac.id",8080);
+  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  Blynk.run();
-  int cm=0;
-  int temp=0;
-  cm = (0.03446 * bacaJarak(D15, D14))/2;
-  temp=cm;
-  sensor=temp;
+  Blynk.run();  // Menjalankan Blynk
+  int temp;     // variable untuk menyimpan nilai integer
+  buttonOn = digitalRead(button1);  // membaca INPUT button1/Limit Switch dan menyimpannya ke variable buttonOn
 
-  if (sensor <= 3 && virtualValue == 1) {
-    delay(5000);
-    myservo.write(180); // move servo to 180•
-    digitalWrite(led_hijau, LOW);
-    tone(buzzer, 13500);
-    delay(1000);
-    tone(buzzer, 0);
-    Serial.print("DATA,");
-    Serial.print(sensor);
-    Serial.print(" cm");
-    Serial.println(" ");
-    delay(5000);
-  } else if (sensor >= 4 && virtualValue == 1) {
-    delay(1000);
-    myservo.write(0);
-    digitalWrite(led_hijau, HIGH);
+//  Serial.print("button on: ");
+//  Serial.println(buttonOn);
+
+  if (virtualValue == 0) {  // Ketika virtualValue yang didapat dari BLYNK(V2) bernilai 0
+    temp = virtualValue;    // temp akan menyimpan nilai virtualValue
+    digitalWrite(ledMerah, HIGH);
+    digitalWrite(ledIjo, LOW);
+    delay(100);
+    ThingSpeak.begin(client); //Inisialisasi ThingSpeak
+    ThingSpeak.writeField(channelID,1,temp,apiTulis);   //Mengirim nilai temp ke thingSpeak channelID monitoring grafik 1
+    Serial.println("Loker locked");
+    myservo.write(180); 
+    toneBunyi();  // Memanggil fungsi toneBunyi() untuk menyalakan buzzer
   }
-  Serial.print("DATA,");
-  Serial.print(sensor);
-  Serial.print(" cm");
-  Serial.println(" ");
-  ThingSpeak.begin(client);
-  ThingSpeak.writeField(channelID,1,sensor,apiTulis);
-  delay(500);
-  //Serial.println(sensor);
+
+  if (buttonOn == 0 && virtualValue == 1){
+    temp = buttonOn;    // temp akan menyimpan nilai buttonOn
+    digitalWrite (ledMerah, HIGH);
+    digitalWrite (ledIjo, LOW);
+    delay(100);
+    Serial.println("Loker locked"); 
+    toneBunyi();  // memanggil fungsi toneBunyi() untuk menyalakan buzzer
+    myservo.write(180);
+    ThingSpeak.begin(client);
+    ThingSpeak.writeField(channelID,1,temp,apiTulis); //Mengirim nilai temp ke thingSpeak channelID monitoring grafik 1
+    delay(100);
+  } else if (buttonOn == 1 && virtualValue == 1) {
+    temp = buttonOn;  // temp akan menyimpan nilai buttonOn
+    digitalWrite (ledMerah, LOW);
+    digitalWrite (ledIjo, HIGH);
+    myservo.write(0);
+    delay(100);
+    Serial.println("Loker unlocked");  
+    ThingSpeak.begin(client);
+    ThingSpeak.writeField(channelID,1,temp,apiTulis); //Mengirim nilai temp ke thingSpeak channelID monitoring grafik 1
+    delay(100);  
+  }
+  delay(1000);
+}
+
+// Fungsi TONE
+void toneBunyi() {
+    tone(buzzer,  13500);   // buzzer menyala dengan frekuensi 13500
+    delay(1000);
+    noTone(buzzer);         // buzzer mati
+    delay(1000);
+}
+
+BLYNK_WRITE(V2) 
+{
+  virtualValue = param.asInt();   // Menyimpan nilai dari V2 yang dirubah menjadi INT ke dalam variable virtualValue
+  if (virtualValue == 1) {
+    toneBunyi();    // Untuk membunyikan buzzer, ketika loker terbuka/ virtualValue bernilai 1
+  }
 }
